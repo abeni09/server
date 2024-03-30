ac507db7f98c6c5ab8dcb001a83cf32c70f86a3d
Abenezer Kindie <abenij09@gmail.com>
2024-03-30 14:04:33 +0300

    updated skipped routes to check for token

--- middleware.js
+++ middleware.js
@@ -3,23 +3,29 @@ require('dotenv').config();
 const secretKey = process.env.SECRET_KEY;
 
 function verifyToken(req, res, next) {  
-    // Skip token verification for login routes
-    if (req.path === '/loginMember' || req.path === '/loginStaff') {
+    // Skip token verification for login routes and uploads
+    const skipVerificationRoutes = ['/loginMember', '/loginStaff', '/processBulkDeposit'];
+    const requestedPath = req.path;
+
+    if (skipVerificationRoutes.includes(requestedPath) || requestedPath.startsWith('/uploads')) {
         return next();
     }
-    const token = req.headers['authorization'];
 
-    if (!token) {
-        return res.status(403).json({ message: 'Token is missing' });
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({ message: 'Authorization header is missing or invalid' });
     }
+
+    const token = authHeader.split(' ')[1];
 
     jwt.verify(token, secretKey, (err, decoded) => {
         if (err) {
-        return res.status(401).json({ message: 'Failed to authenticate token' });
+            return res.status(401).json({ message: 'Failed to authenticate token' });
         } else {
-        // If verification is successful, save the decoded token to the request object for use in other middleware or routes
-        req.user = decoded;
-        next();
+            // If verification is successful, save the decoded token to the request object for use in other middleware or routes
+            req.user = decoded;
+            next();
         }
     });
 }
